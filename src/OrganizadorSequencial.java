4dd9cb3ab3e18231111b78f864d56209ac438682
Eduardo Borges <Eduardo Borges>
2017-02-11 23:38:32 -0300

    adicionado metodos auxiliares e addAluno prototipado

--- src/OrganizadorSequencial.java
+++ src/OrganizadorSequencial.java
@@ -1,9 +1,11 @@
 
+import java.awt.event.ItemEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
+import java.util.function.Function;
 import java.nio.ByteBuffer;
 
 
@@ -17,13 +19,41 @@ class OrganizadorSequencial implements IFileOrganizer {
 		this.canal = raf.getChannel();
 	}
 	
-
 	@Override
-	public void addAluno(Aluno a) {
+	public void addAluno(Aluno alunoToInsert) {
 		try {
-			ByteBuffer buf = Conversor.getBuffer(a);
-			long pos = this.canal.size();
-			this.canal.write(buf, pos);
+			long positionToInsert = this.canal.size() / Aluno.TAM;
+			int itens = (int) (long) (this.canal.size() / Aluno.TAM);
+			System.out.println("> Temos " + itens + " itens");
+
+			if(itens != 0){
+				System.out.println(">> Entrou no if");
+				for (int i = 1; i <= itens; i++) {
+					Aluno currAluno = this.getItemIndex(i);
+					System.out.println(">>> Verificando aluno no index " + i + " com matricula " + currAluno.getMatric());
+					
+					if (alunoToInsert.getMatric() > currAluno.getMatric()) {
+						System.out.println(">>> Aluno atual possui matricula menor, setando ponto de inserção: " + i);
+						positionToInsert = i;
+						break;
+					}
+				}
+
+				System.out.println("> realocar voltando");
+				while (itens > positionToInsert) {
+					System.out.println(">> realocando o item " + itens + " na posicao " + (itens+1) );
+					this.realocateItem(itens, itens + 1);
+					itens--;
+				}
+			}
+
+			System.out.println("> Gravando " + alunoToInsert.getNome() + " na posicao " + positionToInsert);
+			System.out.println("FIM, PROXIMO ITEM");
+			// finalmente gravo
+			long positionToInsertInBytes = positionToInsert * Aluno.TAM;
+			ByteBuffer buf = Conversor.getBuffer(alunoToInsert);
+			this.canal.write(buf,positionToInsertInBytes);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -54,6 +84,12 @@ class OrganizadorSequencial implements IFileOrganizer {
 		return null;
 	}
 	
+	/**
+	 * Retorna a posicao do aluno pela matricula.
+	 * 
+	 * @param matric 	Matricula do aluno buscado
+	 * @return			Retorna um inteiro com a posição daquele aluno
+	 */
 	public int getPosition(long matric){
 		int i = 0;
 		try {
@@ -75,7 +111,7 @@ class OrganizadorSequencial implements IFileOrganizer {
 	}
 	
 	/**
-	 * Retorna o item no banco de dados na posicao passada, como uma array e tals.
+	 * Retorna o item no banco de dados na posicao passada.
 	 * 
 	 * @param index 	Posição do item desejado
 	 * @return 			Retorna o objeto do aluno buscado
@@ -93,7 +129,38 @@ class OrganizadorSequencial implements IFileOrganizer {
 		}
 		return null;
 	}
-	
-	
-	
+
+	public void realocateItem(int source, int target){
+		try {
+			Aluno alunoSource = this.getItemIndex(source);
+			ByteBuffer buf = Conversor.getBuffer(alunoSource);
+			int positionToInsert = (target-1) * Aluno.TAM;
+			this.canal.write(buf, positionToInsert);
+		} catch (Exception e) {
+			System.out.println("Ocorreu um erro ao realocar Aluno: " + e.getMessage());
+		}
+	}
+
+
+	/**
+	 * Printa o estado atual dos dados
+	 */
+	public void showAllData() {
+		System.out.println("\n\n===== Estado dos Dados =======\n");
+		String output = "";
+		try {
+			this.canal.position(0);
+			long size = this.canal.size();
+			while( canal.position() < size){
+				ByteBuffer buf = ByteBuffer.allocate(Aluno.TAM);
+				this.canal.read(buf);
+				Aluno a = Conversor.getAluno(buf);
+				output = output + a.getMatric() + " - ";
+				output = output + a.getNome() + "\n";
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(output);
+	}
 }
--- src/TesteOrganizadorSequencial.java
+++ src/TesteOrganizadorSequencial.java
@@ -4,19 +4,40 @@ public class TesteOrganizadorSequencial {
 
 	public static void main(String[] args) {
 		try{
-			OrganizadorSequencial organizador = new OrganizadorSequencial("alunos.db");
-			
-			Aluno a = new Aluno(20, "Maelsu", "Rua 15", "aloha7@mail.com", (short)22);
-			Aluno b = new Aluno(64, "Eduardo", "Rua 10", "aloha5@mail.com", (short)22);
-			Aluno c = new Aluno(85, "Tonho", "Rua 31", "aloha75@mail.com", (short)22);
-			Aluno d = new Aluno(32, "Luandson", "Rua 11", "aloha5@mail.com", (short)22);
-			
-			organizador.addAluno(a);
-			organizador.addAluno(b);
-			organizador.addAluno(c);
-			organizador.addAluno(d);
 
-			Aluno alunoProcurado = organizador.getAluno(20);
+			Aluno a = new Aluno(1, "Maelsu", "Rua 15", "aloha7@mail.com", (short) 22);
+			Aluno b = new Aluno(2, "Eduardo", "Rua 10", "aloha5@mail.com", (short) 22);
+			Aluno c = new Aluno(4, "Tonho", "Rua 31", "aloha75@mail.com", (short) 22);
+			Aluno d = new Aluno(6, "Luandson", "Rua 11", "aloha5@mail.com", (short) 22);
+			Aluno e = new Aluno(9, "Laisa", "Rua 49", "laisa@email.com", (short)14);
+			Aluno f = new Aluno(3, "Raul", "Rua 11", "laisa@email.com", (short)14);
+
+
+
+			OrganizadorSimples organizadorSimples = new OrganizadorSimples("alunos.db");
+			// organizadorSimples.addAluno(a);
+			// organizadorSimples.addAluno(b);
+			// organizadorSimples.addAluno(c);
+			
+			
+			OrganizadorSequencial organizadorSequencial = new OrganizadorSequencial("alunos.db");
+			
+			
+			organizadorSequencial.addAluno(c);
+			
+			organizadorSequencial.addAluno(e);
+			organizadorSequencial.addAluno(d);
+			organizadorSequencial.addAluno(a);
+			organizadorSequencial.addAluno(b);
+			organizadorSequencial.addAluno(f);
+
+			// // teste de get getPosition
+			// int testePosition = organizador.getPosition(20);
+			// System.out.println(testePosition);
+
+			// // teste de getItemIndex
+			// Aluno alunoIndex = organizador.getItemIndex(1);
+			// System.out.println(alunoIndex.getNome());
 			
 			//System.out.println("Aluno procurado");
 			//System.out.println(alunoProcurado.getNome());
@@ -24,8 +45,10 @@ public class TesteOrganizadorSequencial {
 			//System.out.println("Teste de posição: ");
 			//System.out.println(organizador.getPosition(alunoProcurado.getMatric()));
 			
-			Aluno alunoItem5 = organizador.getItemIndex(2);
-			System.out.println(alunoItem5.getNome());
+			// Aluno alunoItem5 = organizador.getItemIndex(2);
+			// System.out.println(alunoItem5.getNome());
+
+			organizadorSequencial.showAllData();
 			
 		}
 		catch(FileNotFoundException e){
