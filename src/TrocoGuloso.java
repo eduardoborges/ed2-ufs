8a30b354ea581ca9d8ee413f9727b76c8d7b24fa
Eduardo <eduardodmais.borges@gmail.com>
2017-02-24 19:45:23 -0300

    Implementacao do calculo de moedas de troco

---
+++ src/TrocoGuloso.java
@@ -0,0 +1,25 @@
+class TrocoGuloso{
+	
+	private static final int[] moedas = new int[]{ 100,50,25,10,5,1 };
+
+	public static void troco(int valor){
+
+		int moedas_usadas = 0;
+
+		for (int moeda:moedas) {
+			if( valor >= moeda){
+				while( valor >= moeda ){
+					valor = valor - moeda;
+					moedas_usadas += 1;
+				}
+				System.out.println(moedas_usadas + " moedas de " + moeda);
+				moedas_usadas = 0;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		TrocoGuloso.troco(7);
+	}
+}
+
