bf534a65bbaf9386edf7cee233cbfbefe4b1c3e7
Eduardo <eduardodmais.borges@gmail.com>
2017-02-24 20:08:19 -0300

    Outro metodo de troco mais exchuto

--- src/TrocoGuloso.java
+++ src/TrocoGuloso.java
@@ -1,11 +1,9 @@
 class TrocoGuloso{
 	
-	private static final int[] moedas = new int[]{ 100,50,25,10,5,1 };
+	private static final int[] moedas = new int[]{ 100,50,25,20,10,5,1 };
 
 	public static void troco(int valor){
-
 		int moedas_usadas = 0;
-
 		for (int moeda:moedas) {
 			if( valor >= moeda){
 				while( valor >= moeda ){
@@ -18,8 +16,17 @@ class TrocoGuloso{
 		}
 	}
 
+	public static void trocoMetodo2(int valor){
+		for (int moeda:moedas) {
+			if(valor >= moeda)
+				System.out.println(valor/moeda + " moedas de " + moeda);
+				valor = valor % moeda;
+		}
+	}
+
+
 	public static void main(String[] args) {
-		TrocoGuloso.troco(7);
+		TrocoGuloso.trocoMetodo2(90);
 	}
 }
 
