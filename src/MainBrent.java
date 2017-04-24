27e09b2520ee0179e6f9f7d15fc7cdfd9bb995f4
Eduardo Borges <eduardodmais.borges@gmail.com>
2017-04-23 23:45:33 -0300

    Implementaçao completa do metodo de Brent.

--- src/MainBrent.java
+++ src/MainBrent.java
@@ -1,151 +1,43 @@
-/* *****************************************************************************
- * Códigos desenvolvidos pelos seguintes alunos
- *
- * @author Claudson Bispo Martins Santos    201410042132
- * @author Edgar Vieira Lima Neto           201410042150
- * @author Allan silva Santos               201410060220
- * ****************************************************************************/
 
 import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
+
 
 public class MainBrent {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws FileNotFoundException,
-                                                  IOException {
-        // Obtém o momento em que o algoritmo começou a ser processado
-        // long startTime = System.currentTimeMillis();
-        SimpleDateFormat ft = new SimpleDateFormat("HH:mm:ss");
-        Date startTime = new Date();
-        System.out.println("Início em: " + ft.format(startTime));
-        
-        // Instancia manualmente 3 alunos
+    public static void main(String[] args) throws FileNotFoundException {
+
+    
         Aluno a = new Aluno(78, "Maelsu", "Rua 15", "aloha7@mail.com", (short) 22);
         Aluno b = new Aluno(16, "Eduardo", "Rua 10", "aloha5@mail.com", (short) 22);
         Aluno c = new Aluno(89, "Tonho", "Rua 31", "aloha75@mail.com", (short) 22);
         Aluno d = new Aluno(1, "Luandson", "Rua 11", "aloha5@mail.com", (short) 22);
         Aluno e = new Aluno(0, "Laisa", "Rua 49", "laisa@email.com", (short)14);
         Aluno f = new Aluno(15, "Raul", "Rua 11", "laisa@email.com", (short)14);
-        Aluno vazio = new Aluno(-1, "[VAZIO]", "[VAZIO]", "[VAZIO]", (short) 0);
+        
 
         String arquivo = "brentDatabase.db";
         OrganizadorBrent database = new OrganizadorBrent(arquivo);
-        //teste.inicializaArquivo(vazio);
-        
-
-        // Persiste os alunos instanciados manualmente no arquivo
+        // database.initDatabase();
         database.addAluno(a);
         database.addAluno(b);
         database.addAluno(c);
         database.addAluno(d);
         database.addAluno(e);
         database.addAluno(f);
-        database.listarArquivo();
-        
-        Aluno testeA = database.getAluno(29);
-        if (testeA != null) System.out.println(testeA.getMatric() + " | " + 
-                testeA.getNome().substring(0,15) + " | " +
-                testeA.getEmail());
-        
-        Aluno del = database.delAluno(27);
-        if (del != null)
-            System.out.println("O aluno " + del.getNome() + " (" + del.getMatric() + ") foi removido.");
-        
-        database.listarArquivo();
+        database.showAllData();
         
         
-        // Instancia e persiste automaticamente 1 Milhão de registros no arquivo
-        /*Aluno novo = null;
-        for (int i = 1; i <= 1000000; i++) {
-            novo = new Aluno(i, String.valueOf(i), "Rua 6, 45", (short) 14, "M", "email@com");
-            teste.addAluno(novo);
-        }*/
+        // Aluno testeA = database.getAluno(29);
+        // if (testeA != null) System.out.println(testeA.getMatric() + " | " + 
+        //         testeA.getNome().substring(0,15) + " | " +
+        //         testeA.getEmail());
         
-        // Lê as matriculas a serem buscadas
-        /*ManipuladorSequencial teste2 = new ManipuladorSequencial("data\\selected.db");
-        int[] selected = teste2.lerSelecionados();*/
+        // Aluno del = database.delAluno(27);
+        // if (del != null)
+        //     System.out.println("O aluno " + del.getNome() + " (" + del.getMatric() + ") foi removido.");
         
-        // Realiza a busca sequencial das matriculas
-        /*for (int i = 0; i < 1000; i++) {
-            Aluno a = teste.getAluno(selected[i]);
-            if (a != null) System.out.println("[ " + i + " ] " +
-                    a.getMatricula() + " | " + 
-                    a.getNome().substring(0,15) + " | " +
-                    a.getEmail());
-        }*/
-        
-        // Migra os alunos do ENEM_ALEAT para o ENEM_BRENT
-        // ManipuladorSequencial teste2 = new ManipuladorSequencial("data\\enem_aleat.db");
-        // for (int i = 0; i < 8722356; i++) {
-        //     Aluno a = new Aluno(teste2.lerAluno(i * 157));
-        //     System.out.println("[ " + i + " ] " +
-        //             a.getMatricula() + " | " + 
-        //             a.getNome().substring(0,15) + " | " +
-        //             a.getEmail());
-        //     teste.addAluno(a);
-        // }
-  
-        // Obtém do arquivo os alunos que foram instanciados manualmente
-        /*Aluno b = teste.getAluno(8);
-        if (b != null) System.out.println(b.getMatricula() + " | " + 
-                                          b.getNome().substring(0,15) + " | " +
-                                          b.getEmail());
-        b = teste.getAluno(15);
-        if (b != null) System.out.println(b.getMatricula() + " | " + 
-                                          b.getNome().substring(0,15) + " | " +
-                                          b.getEmail());
-        b = teste.getAluno(7);
-        if (b != null) System.out.println(b.getMatricula() + " | " + 
-                                          b.getNome().substring(0,15) + " | " +
-                                          b.getEmail());*/
+        // database.showAllData();
 
-        // Deleta do arquivo os alunos que foram instanciados manualmente
-        /*Aluno del = teste.delAluno(8);
-        if (del != null)
-            System.out.println("O aluno " + del.getNome() +
-                               " (" + del.getMatricula() + ") foi removido.");
-        del = teste.delAluno(15);
-        if (del != null)
-            System.out.println("O aluno " + del.getNome() +
-                               " (" + del.getMatricula() + ") foi removido.");
-        del = teste.delAluno(7);
-        if (del != null)
-            System.out.println("O aluno " + del.getNome() +
-                               " (" + del.getMatricula() + ") foi removido.");*/
-
-        // Obtém o momento em que o algoritmo terminou de ser processado
-        // long endTime   = System.currentTimeMillis();
-        Date endTime = new Date();
-        // Calcula e imprime o tempo total de execução em milisegundos
-        long totalTime = endTime.getTime() - startTime.getTime();
-        
-        // Computa a diferença detalhadamente
-        List<TimeUnit> units = new ArrayList<TimeUnit>(EnumSet.allOf(TimeUnit.class));
-        Collections.reverse(units);
-        Map<TimeUnit,Long> result = new LinkedHashMap<TimeUnit,Long>();
-        long milliSecondsRest = totalTime;
-        for (TimeUnit unit : units) {
-            long diff = unit.convert(milliSecondsRest,TimeUnit.MILLISECONDS);
-            long diffInMilliSecondsForUnit = unit.toMillis(diff);
-            milliSecondsRest = milliSecondsRest - diffInMilliSecondsForUnit;
-            result.put(unit,diff);
-        }
-        
-        System.out.println("---------------------");
-        System.out.println("Hora de Início:    " + ft.format(startTime));
-        System.out.println("Hora do Término:   " + ft.format(endTime));
-        System.out.println("");
-        System.out.println("Tempo de Execução: " + totalTime + " ms");
-        /*System.out.println("                   " + (totalTime/1000) + " s");
-        System.out.println("                   " +
-                result.get(TimeUnit.HOURS) + " h : " +
-                result.get(TimeUnit.MINUTES) + " min : " +
-                result.get(TimeUnit.SECONDS) + " s");*/
     }
     
 }
--- src/OrganizadorBrent.java
+++ src/OrganizadorBrent.java
@@ -9,54 +9,67 @@
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 class OrganizadorBrent implements IFileOrganizer {
+    
+    private final int SIZE = 12000017;
 
-    // TAManho da tabela de registros do arquivo
-    private final int P = 1200; 
-    
-    // Numero de bytes que um registro ocupa
-    private final int TAM = 200;
-    
     // Canal de comunicacao com o arquivo
     private FileChannel canal;
     
-    public OrganizadorBrent(String path) throws FileNotFoundException{
-        File file = new File(path);
-        RandomAccessFile rf = new RandomAccessFile(file, "rw");
-        this.canal = rf.getChannel();
+    public OrganizadorBrent(String fileName) throws FileNotFoundException{
+
+        File file = new File(fileName);
+		RandomAccessFile raf = new RandomAccessFile(file, "rw");
+		this.canal = raf.getChannel();
+        
+    }
+    /**
+	 * Adiciona um Aluno no arquivo
+	 * 
+	 * @param matricula		Matricula do Aluno a ser processado
+	 * @return hash         Posicao hash a ser inserido
+	 **/
+    private int calcHash(long matricula) {
+        return (int)(matricula % this.SIZE);
     }
     
-    private int calculaHash(long matricula) {
-        return (int)(matricula % this.P);
+    /**
+	 * Calcula o incremento do registro Aluno
+	 * 
+	 * @param matricula		Matricula do Aluno a ser processado
+	 * @return increment    O incremento da posicao do aluno
+     **/
+    private int calcIncrement(long matricula) {
+        return (int)(matricula % (this.SIZE - 2)) + 1;
     }
     
-    private int calculaIncremento(long matricula) {
-        return (int)(matricula % (this.P - 2)) + 1;
-    }
-    
+    /**
+	 * Adiciona um Aluno no arquivo
+	 * 
+	 * @param alunoToInsert		Objeto com o aluno a ser inserido
+	 * @return void
+	 **/
     @Override
     public void addAluno(Aluno a) {
-        ByteBuffer buf = ByteBuffer.allocate(this.TAM);
+        ByteBuffer buf = ByteBuffer.allocate(Aluno.TAM);
         
         long matric     = a.getMatric();
-        int hash        = calculaHash(matric);
-        int posicao     = hash * this.TAM;
+        int hash        = calcHash(matric);
+        int posicao     = hash * Aluno.TAM;
         int posicao2    = posicao;
         int posicao3    = posicao;
-        long x, z;
+        int x, z;
 
         try {
             canal.position(posicao);
             canal.read(buf);
             buf.flip();
-            x = buf.getLong();
+            x = (int)buf.getLong();
             z = x;
             buf.clear();
             
-            // Se a posicao estiver livre
+            // Se a posicao estiver livre DEUS AJUDE QUE SIM AMÉM?
             if (x == 0 || x == -1) {
                 canal.position(posicao);
                 canal.write(Conversor.getBuffer(a));
@@ -65,40 +78,45 @@ class OrganizadorBrent implements IFileOrganizer {
 
             // Houve uma colisao
             else {
-                //SIMULANDO OS PASSOS PARA ADD O NOVO REGISTRO SEM MOVER O PRIMEIRO
-                int colisao = (int)(x);
-                int incremento = calculaIncremento(matric);
+                // add sem mover o primeiro
+                int colisao = x;
+                int incremento = calcIncrement(matric);
                 int passos = 1;
                 while(x != 0 && x != -1) {
                     passos++;
-                    posicao = (((posicao/this.TAM) + incremento) % this.P) * this.TAM;
+                    posicao = (((posicao/Aluno.TAM) + incremento) % this.SIZE) * Aluno.TAM;
                     canal.position(posicao);
                     canal.read(buf);
                     buf.flip();
-                    x = buf.getInt();
+                    x = (int)(buf.getLong());
                     buf.clear();
                 }
                 
-                //SIMULANDO OS PASSOS PARA ADD O NOVO REGISTRO MOVENDO O PRIMEIRO
-                int incremento2 = calculaIncremento(colisao);
+                // add movendo o primeiro
+                int incremento2 = calcIncrement(colisao);
                 int passos2 = 1;
                 while(z != 0 && z != -1) {
                     passos2++;
-                    posicao2 = (((posicao2/this.TAM) + incremento2) % this.P) * this.TAM;
+                    posicao2 = (((posicao2/Aluno.TAM) + incremento2) % this.SIZE) * Aluno.TAM;
                     canal.position(posicao2);
                     canal.read(buf);
                     buf.flip();
-                    z = buf.getInt();
+                    z = (int)(buf.getLong());
                     buf.clear();
                 }
                 
-                // Opcao I: apenas escrever na posicao encontrada
-                if ( (custoBusca(colisao)+passos) <= (custoBusca(colisao)+passos2) ) { //+1 ja incluso
+                // AQUI TEMOS DUAS OPCOES
+                // 1 - apenas escrever ou
+                // 2 - colocar em outro no inicio e mover o que está lá
+
+
+                // 1
+                if ( (searchCost(colisao)+passos) <= (searchCost(colisao)+passos2) ) {
                     canal.position(posicao);
                     canal.write(Conversor.getBuffer(a));
                     buf.clear();
                 }
-                // Opcao II: por o novo no inicio e mover o original
+                // 2
                 else {
                     canal.position(posicao3);
                     canal.read(buf);
@@ -113,37 +131,42 @@ class OrganizadorBrent implements IFileOrganizer {
             }
 
         } catch (IOException ex) {
-            Logger.getLogger(OrganizadorSequencial.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
         System.out.println("Aluno adicionado!");
     }
 
+    /**
+	 * Busca um aluno do arquivo passando sua matricula
+	 * 
+	 * @param 	matric	Matricula do aluno a ser buscado
+	 * @return 	aluno	Um objeto Aluno com o aluno encontrado, caso contrario retorna null
+	 **/
     @Override
     public Aluno getAluno(long matric) {
-        ByteBuffer buf = ByteBuffer.allocate(this.TAM);
+        ByteBuffer buf = ByteBuffer.allocate(Aluno.TAM);
         
-        int hash = calculaHash(matric);
-        int posicao = (int)(hash * this.TAM);
-        long x, z;
+        int hash = this.calcHash(matric);
+        int posicao = (hash * Aluno.TAM);
 
         try {
             canal.position(posicao);
             canal.read(buf);
             buf.flip();
-            x = buf.getLong();
+            int x = (int)buf.getLong();
             buf.clear();
             if (x == matric) {
                 Aluno a = Conversor.getAluno(buf);
                 return a;
             }
             else {
-                int incremento = calculaIncremento(matric);
-                while(x != 0) {
-                    posicao = (((posicao/this.TAM) + incremento) % this.P) * this.TAM;
+                int incremento = this.calcIncrement(matric);
+                while(x != 0 || x != -1) {
+                    posicao = (((posicao/Aluno.TAM) + incremento) % this.SIZE) * Aluno.TAM;
                     canal.position(posicao);
                     canal.read(buf);
                     buf.flip();
-                    x = buf.getLong();
+                    x = (int)buf.getLong();
                     buf.clear();
                     if (x == matric) {
                         Aluno a = Conversor.getAluno(buf);
@@ -156,40 +179,46 @@ class OrganizadorBrent implements IFileOrganizer {
         }
         return null;
     }
-    
+    /**
+	 * Remove um aluno do arquivo passando sua matricula
+	 * 
+	 * @param 	matric	Matricula do aluno a ser removido
+	 * @return 	aluno	Um objeto com o al	uno adicionado
+	 **/
     @Override
     public Aluno delAluno(long matric) {
-        ByteBuffer buf = ByteBuffer.allocate(this.TAM);
+        ByteBuffer buf = ByteBuffer.allocate(Aluno.TAM);
         // Informacoes do aluno removido
         Aluno removido = null;
-        int hash = calculaHash(matric);
-        int posicao = hash * this.TAM;
+        int hash = this.calcHash(matric);
+        int posicao = hash * Aluno.TAM;
+        Aluno remAluno = new Aluno(-1, "[REMOVIDO]", "[REMOVIDO]", "[REMOVIDO]", (short) 0);
         
         try {
             canal.position(posicao);
             canal.read(buf);
             buf.flip();
-            int x = buf.getInt();
+            int x = (int)buf.getLong();
             buf.clear();
             if (x == matric) {
                 removido = Conversor.getAluno(buf);
                 canal.position(posicao);
-                canal.write(removedByteBuffer());
+                canal.write(Conversor.getBuffer(remAluno));
                 return removido;
             }
             else {
-                int incremento = calculaIncremento(matric);
+                int incremento = calcIncrement(matric);
                 while(x != 0) {
-                    posicao = (((posicao/this.TAM) + incremento) % this.P) * this.TAM;
+                    posicao = (((posicao/Aluno.TAM) + incremento) % this.SIZE) * Aluno.TAM;
                     canal.position(posicao);
                     canal.read(buf);
                     buf.flip();
-                    x = buf.getInt();
+                    x = (int)buf.getLong();
                     buf.clear();
                     if (x == matric) {
                         removido = Conversor.getAluno(buf);
                         canal.position(posicao);
-                        canal.write(removedByteBuffer());
+                        canal.write(Conversor.getBuffer(remAluno));
                         return removido;
                     }
                 }
@@ -203,42 +232,57 @@ class OrganizadorBrent implements IFileOrganizer {
     
     // helpers functions
 
-    private ByteBuffer removedByteBuffer() {
-        ByteBuffer b = ByteBuffer.allocate(this.TAM);
-        b.putInt(-1);
-        b.put("".getBytes());
-        b.put("".getBytes());
-        b.putShort((short) 0);
-        b.put("".getBytes());
-        b.put("".getBytes());
-        b.flip();
-        return b;
+
+    /**
+     * Inicializa a base com campos vazios.
+     * Isso é necessário porque vc vai acessor campos
+     * e eles podem não existir fisicamente, então
+     * cria logo os bagui tudo vazio e vai que é tua.
+     * 
+     * @return void
+     */
+    public void initDatabase() {        
+        try {
+            canal.position(0);
+            for (int i = 0; i < this.SIZE; i++){
+                Aluno blankAluno = new Aluno(-1, "[VAZIO]", "[VAZIO]", "[VAZIO]", (short) 0);
+                canal.write(Conversor.getBuffer(blankAluno));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-    private int custoBusca(int matricula) {
-        ByteBuffer buf = ByteBuffer.allocate(this.TAM);
+
+    /** 
+     * Calcula o custo da busca de um Aluno na base
+     * 
+     * @param matricula     Matricula do aluno a ser processado
+     * @return passos       Um inteiro com a quantidade de passos para executar a instruçao.
+     */
+    private int searchCost(int matricula) {
+        ByteBuffer buf = ByteBuffer.allocate(Aluno.TAM);
         
-        int hash = calculaHash(matricula);
-        int posicao = hash * this.TAM;
-        int passos = 1;
+        int hash    = calcHash(matricula);
+        int posicao = hash * Aluno.TAM;
+        int passos  = 1;
         
         try {
             canal.position(posicao);
             canal.read(buf);
             buf.flip();
-            long x = buf.getLong();
+            int x = (int)(buf.getLong());
             buf.clear();
             
             if (x == matricula) return passos;
             else {
-                int incremento = calculaIncremento(matricula);
+                int incremento = this.calcIncrement(matricula);
                 while(x != 0) {
                     passos++;
-                    posicao = (((posicao/this.TAM) + incremento) % this.P) * this.TAM;
+                    posicao = (((posicao/Aluno.TAM) + incremento) % this.SIZE) * Aluno.TAM;
                     canal.position(posicao);
                     canal.read(buf);
                     buf.flip();
-                    x = buf.getLong();
+                    x = (int)buf.getLong();
                     buf.clear();
                     if (x == matricula) break; 
                 }
@@ -248,31 +292,30 @@ class OrganizadorBrent implements IFileOrganizer {
         }
         return passos;
     }
-    
-    public void inicializaArquivo(Aluno vazio) {        
-        try {
-            canal.position(0);
-            for (int i = 0; i < this.P; i++)
-                canal.write(Conversor.getBuffer(vazio));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public void listarArquivo() {
-        ByteBuffer buf = ByteBuffer.allocate(this.TAM);
-        try {
-            canal.position(0);
-            for (int i = 0; i < this.P; i++) {
-                canal.read(buf);
-                buf.flip();
-                int x = buf.getInt();
-                System.out.println("[ " + i + " ] " + x);
-                buf.clear();
+
+    /**
+	 * Printa o estado atual dos dados do arquivo
+     * 
+     * @return void
+	 */
+    public void showAllData() {
+		System.out.println("\n\n= ESTADO DA BASE DE DADOS =======\n");
+		try {
+            for (int i = 0; i < this.SIZE; i++){
+                
+                ByteBuffer buf = ByteBuffer.allocate(Aluno.TAM);
+				this.canal.read(buf);
+				Aluno a = Conversor.getAluno(buf);
+                String output = "-----------------------------------------\n";
+                output += "| "+i+" |";
+				output += a.getMatric() + " | ";
+				output += a.getNome() + "\n";
+                output += "-----------------------------------------\n";
+                System.out.println(output);
+
             }
-        } catch (IOException ex) {
-            Logger.getLogger(OrganizadorSequencial.class.getName())
-                    .log(Level.SEVERE, null, ex);
-        }
-    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
